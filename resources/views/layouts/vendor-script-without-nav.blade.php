f836aed242fa0679e7a4ffec3b392e2454fbca23
Hector Aldahir Lopez R. <hectoralr21@gmail.com>
2024-09-14 22:34:07 -0600

    cambios

--- resources/views/layouts/vendor-script-without-nav.blade.php
+++ resources/views/layouts/vendor-script-without-nav.blade.php
@@ -1,12 +1,11 @@
 <!-- JAVASCRIPT -->
 <script src="{{ URL::asset('build/libs/jquery/jquery.min.js') }}"></script>
 <script src="{{ URL::asset('build/libs/bootstrap/js/bootstrap.min.js') }}"></script>
-<script src="{{ URL::asset('build/libs/metismenu/metismenu.min.js') }}"></script>
 <script src="{{ URL::asset('build/libs/simplebar/simplebar.min.js') }}"></script>
 <script src="{{ URL::asset('build/libs/node-waves/waves.min.js') }}"></script>
 <script src="{{ URL::asset('build/libs/feather-icons/feather.min.js') }}"></script>
 <!-- pace js -->
-<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/metisMenu/3.0.7/metisMenu.min.js"></script>-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/metisMenu/3.0.7/metisMenu.min.js"></script>
 
 <script src="{{ URL::asset('build/libs/pace-js/pace.min.js') }}"></script>
 @yield('script')
